076abc768abcee11e1f1274ca7b866a704397e51
neominal.solutions <neominal.solutions@gmail.com>
2025-12-20 14:51:19 +0300

    feat: update ProductsPage to navigate to cart summary and refactor add to cart functions for better clarity

--- src/pages/product/products.page.tsx
+++ src/pages/product/products.page.tsx
@@ -9,12 +9,14 @@ import type { AppDispatch } from '../../store/store';
 
 // addItem -> export isimleri aynı olduğu için çakışmayı önlemek adına yeniden adlandırıyoruz.
 import { addItem as _addItem } from '../../store/cart/cart.slice';
+import { useNavigate } from 'react-router';
 
 const ProductsPage = () => {
 	// component üzerinden state değiştirecek olan bir action tetiklememiz gerekiyor.
 	// useContext hook ile context listener görevi görür.
 	const { addItem } = useContext(CartContext) as CartContextType;
 	const dispatch = useDispatch<AppDispatch>();
+	const navigate = useNavigate();
 
 	const data: Product[] = [
 		{
@@ -35,7 +37,7 @@ const ProductsPage = () => {
 		},
 	];
 
-	const onAddtoCart = (data: Product) => {
+	const onAddtoCartContextAPI = (data: Product) => {
 		console.log(`Product ${data.id} added to cart.`);
 		// ilgili methoda  -> redux içtin action'a gönderilen veriye payload denir
 		// payload: state değiştirmek için action ile gönderilen veri
@@ -51,49 +53,64 @@ const ProductsPage = () => {
 		// cartProvider içindeki addItem methodunu kullanarak ürünü sepete eklemeliyiz.
 	};
 
+	const onAddtoCartReduxToolkit = (data: Product) => {
+		dispatch(
+			_addItem({
+				productId: data.id,
+				name: data.name,
+				quantity: 1,
+				price: data.price,
+			})
+		);
+		window.alert(`${data.name} sepete eklendi.`);
+
+		// cartProvider içindeki addItem methodunu kullanarak ürünü sepete eklemeliyiz.
+	};
+
 	return (
-		<div className="container mx-auto p-4">
-			<h1>Products List</h1>
-			{data.map((product) => (
-				<div
-					className="grid gap-4 p-4 mb-2 border rounded shadow "
-					key={product.id}
-				>
-					<h2 className="text-xl font-semibold">{product.name}</h2>
-					<p className="text-gray-600">{product.description}</p>
-					<p className="text-green-600">Price: ${product.price}</p>
-					<p className="text-blue-600">In Stock: {product.stockQuantity}</p>
+		<>
+			<div className="container mx-auto p-4">
+				<h1>Products List</h1>
+				<div>
 					<button
-						onClick={() => onAddtoCart(product)}
-						className="px-4 py-2 bg-blue-600 text-white font-medium rounded-lg hover:bg-blue-700 transition"
+						onClick={() => navigate('/cart-summary')}
+						className="mt-4 px-4 py-2 bg-gray-600 text-white font-medium rounded-lg hover:bg-gray-700 transition mb-2"
 					>
-						Sepete Ekle (Context API)
+						Sepete Git (Context API)
 					</button>
 					<button
-						onClick={() =>
-							dispatch(
-								_addItem({
-									productId: product.id,
-									name: product.name,
-									quantity: 1,
-									price: product.price,
-								})
-							)
-						}
-						className="px-4 py-2 bg-blue-600 text-white font-medium rounded-lg hover:bg-blue-700 transition"
+						onClick={() => navigate('/cart-summary-v2')}
+						className="mt-4 px-4 py-2 bg-blue-600 text-white font-medium rounded-lg hover:bg-blue-700 transition mb-2 ml-2"
 					>
-						Sepete Ekle (Redux Toolkit)
+						Sepete Git (Redux Toolkit)
 					</button>
 				</div>
-			))}
 
-			<button
-				onClick={() => {}}
-				className="mt-4 px-4 py-2 bg-green-600 text-white font-medium rounded-lg hover:bg-green-700 transition"
-			>
-				Sepete Git
-			</button>
-		</div>
+				{data.map((product) => (
+					<div
+						className="grid gap-4 p-4 mb-2 border rounded shadow "
+						key={product.id}
+					>
+						<h2 className="text-xl font-semibold">{product.name}</h2>
+						<p className="text-gray-600">{product.description}</p>
+						<p className="text-green-600">Price: ${product.price}</p>
+						<p className="text-blue-600">In Stock: {product.stockQuantity}</p>
+						<button
+							onClick={() => onAddtoCartContextAPI(product)}
+							className="px-4 py-2 bg-blue-600 text-white font-medium rounded-lg hover:bg-blue-700 transition"
+						>
+							Sepete Ekle (Context API)
+						</button>
+						<button
+							onClick={() => onAddtoCartReduxToolkit(product)}
+							className="px-4 py-2 bg-blue-600 text-white font-medium rounded-lg hover:bg-blue-700 transition"
+						>
+							Sepete Ekle (Redux Toolkit)
+						</button>
+					</div>
+				))}
+			</div>
+		</>
 	);
 };
 
